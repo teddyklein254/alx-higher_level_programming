15524c05b33e4ee52e8291910c35bcbd928fed86
Teddy <papateddyetyanga2gmail.com>
2024-01-21 07:22:07 -0800

    code

---
+++ 0x05-python-exceptions/101-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+safe_function = __import__('101-safe_function').safe_function
+
+
+def my_div(a, b):
+    return a / b
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
